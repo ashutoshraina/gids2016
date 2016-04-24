60f14af17d5606972a2996ed7ff05c714b304cb8
Ashutosh Raina <Ashutosh Raina>
2016-04-24 19:41:47 +0530

    Reomved non demo code.
     Committer: Ashutosh Raina <Ashutosh Raina>

--- RefinedWay/Program.cs
+++ RefinedWay/Program.cs
@@ -9,15 +9,8 @@ namespace RefinedWay
             //var user = new User { Ip = "10.12.3.4", Message = "bad" };
             //var firstResult = ChatBotValidator.IsUsingBannedWords(user);
             //firstResult.Select(r => r);
-            
-            RefinedValidator();
 
             Console.ReadLine();
-        }
-
-        static void RefinedValidator()
-        {
-            new CustomerService(new CustomerRepository()).Delete(-1);
         }
     }
 }
